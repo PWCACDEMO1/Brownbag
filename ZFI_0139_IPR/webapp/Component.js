00e64b0220e3b73e10abcf0d555f8a025aee8d11
Pramod S J <pramod.s.j@insdc01.pwc.com>
2022-06-16 10:31:06 +0000

    Complete Changed

--- ZFI_0139_IPR/webapp/Component.js
+++
@@ -1,7 +0,0 @@
-sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
-	return AppComponent.extend("ZFI_0139_IPR.zfi_0139_ipr.Component", {
-		metadata: {
-			"manifest": "json"
-		}
-	});
-});
