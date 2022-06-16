cd985daddd8d5d27052ae14ec275f1392abb830e
Pramod S J <pramod.s.j@insdc01.pwc.com>
2022-06-16 10:21:57 +0000

    Initital Commit

---
+++ ZFI_0139_IPR/webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend("ZFI_0139_IPR.zfi_0139_ipr.Component", {
+		metadata: {
+			"manifest": "json"
+		}
+	});
+});
